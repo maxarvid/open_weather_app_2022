cee020125a383950560f235d7515a1e4a55e542b
maxarvid <max.arvid.anderson@gmail.com>
2022-02-15 14:33:05 +0100

    WIP adds geolocation request on render

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  return (
-    <div>Hello world</div>
-  )
-}
+  const [coords, setCoords] = useState([]);
 
-export default App
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      let { latitude, longitude } = position.coords;
+      setCoords([latitude, longitude]);
+      debugger;
+      // Query API to get location
+      
+      // Query the Weather API to get temperature
+    });
+  }, []);
+
+  return (
+    <div data-cy="weather-display">
+      <h4 data-cy="temp">Temperature: </h4>
+      <h4 data-cy="city">City: </h4>
+    </div>
+  );
+};
+
+export default App;
